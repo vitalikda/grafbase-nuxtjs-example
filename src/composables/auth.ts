812231fd21ffd510fdef4fe811a8bf8b61ed90f0
vitalikda <vitalikda@users.noreply.github.com>
2022-08-02 12:55:15 +0000

    Prettified Code!

--- src/composables/auth.ts
+++ src/composables/auth.ts
@@ -1,34 +1,35 @@
-export const useGithubCookie = () => useCookie('gh_token');
+export const useGithubCookie = () => useCookie("gh_token");
 
 export const githubFetch = (url: string, fetchOptions: any = {}) => {
   return $fetch(url, {
-
-    baseURL: 'https://api.github.com',
+    baseURL: "https://api.github.com",
     ...fetchOptions,
     headers: {
       Authorization: `token ${useGithubCookie().value}`,
-      ...fetchOptions.headers 
-    }
-  })
-}
+      ...fetchOptions.headers,
+    },
+  });
+};
 
 export const useGithubUser = async () => {
-  const cookie = useGithubCookie()
-  const user = useState('gh_user')
+  const cookie = useGithubCookie();
+  const user = useState("gh_user");
   if (cookie.value && !user.value) {
-    user.value = await githubFetch('/user')
+    user.value = await githubFetch("/user");
   }
-  return user
-}
+  return user;
+};
 
 export const githubLogin = () => {
   if (process.client) {
-    const { githubClientId } = useRuntimeConfig()
-    window.location.replace(`https://github.com/login/oauth/authorize?client_id=${githubClientId}&scope=public_repo`)
+    const { githubClientId } = useRuntimeConfig();
+    window.location.replace(
+      `https://github.com/login/oauth/authorize?client_id=${githubClientId}&scope=public_repo`
+    );
   }
-}
+};
 
 export const githubLogout = async () => {
-  useGithubCookie().value = ''
-  useState('gh_user').value = null
-}
+  useGithubCookie().value = "";
+  useState("gh_user").value = null;
+};
